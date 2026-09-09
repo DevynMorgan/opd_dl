e123a27c18d063b4bb87fad49492482eeca47f75
DevynMorgan <kaela.frosty2@gmail.com>
2026-09-09 16:07:02 -0500

    Load database interface styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./additional.css";
 
 export const metadata: Metadata = {
   title: "Opaline Police Department | Records Terminal",
