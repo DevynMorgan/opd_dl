0f4b146488adf80cf8c11350f9742fb7a365feb3
DevynMorgan <kaela.frosty2@gmail.com>
2026-09-11 11:49:19 -0500

    Load stable case panel styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./additional.css";
+import "./case-panels.css";
 import AuthGate from "./auth-gate";
 import LogoutHandler from "./logout-handler";
 import AdminEnhancer from "./admin-enhancer";
